108756617657926edb4befaa25f398fcc1ff69b8
elenavondermeulen <elenavonder20@outlook.com>
2020-12-05 15:15:54 -0500

    * .gitignore: initial commit
    * Program.cs: initial commit
    * FunWith.NET5.csproj:initial commit
    * FunWith.NET5Solution.sln: initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunWith.NET5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
