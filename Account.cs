599b3e4b3d2821346333ffd9bc0e8587fa3afd3c
elenavondermeulen <elenavonder20@outlook.com>
2020-12-05 16:16:25 -0500

    Added class

---
+++ Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FunWith.NET5
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public Account()
+        {
+        }
+    }
+}
