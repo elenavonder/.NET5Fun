9955529ca7a6a81d916863b2a441a701dff51c46
elenavondermeulen <elenavonder20@outlook.com>
2020-12-14 18:20:37 -0500

    added properties

--- Class.cs
+++ Class.cs
@@ -3,6 +3,17 @@ namespace FunWith.NET5
 {
     public class Class
     {
+        private static int NextId = 1;
+
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public int Section { get; set; }
+        public int? InstructorId { get; set; }
+
+        public void print()
+        {
+
+        }
         public Class()
         {
         }
