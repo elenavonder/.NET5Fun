bab41c7219728e49b24c93ce35e31a786cce67f0
elenavonder <evonder13@gmail.com>
2020-12-14 18:31:40 -0500

    Added new instance

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace FunWith.NET5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WHy won't this commit?");
+            var cls1 = new Class("English", 101, null);
+            Console.WriteLine($"{cls1.Id}, {cls1.Subject}");
         }
     }
 }
