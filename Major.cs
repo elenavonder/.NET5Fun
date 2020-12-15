d77aaed5b98a8b3f6b377d0ac6d558bf53a372fa
elenavonder <evonder13@gmail.com>
2020-12-14 20:32:50 -0500

    added new methods

---
+++ Major.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace FunWith.NET5
+{
+    public class Major
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int MinSAT { get; set; }
+
+        public void SetMinSAT(int minsat)
+        {
+            if (minsat < 400 || minsat > 1600)
+            {
+                Console.WriteLine($"ERROR minsat outside bounds");
+                return;
+            }
+            this.MinSAT = minsat;
+        }
+
+        public Major (int id, string desc, int MinSAT)
+        {
+            this.Id = id;
+            this.Description = desc;
+            this.MinSAT = MinSAT = 400;
+            this.SetMinSAT(MinSAT);
+        }
+        
+        public Major()
+        {
+        }
+    }
+}
