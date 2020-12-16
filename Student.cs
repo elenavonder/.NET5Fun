373840d64c136fe9419918a83d821832041fa453
elenavonder <evonder13@gmail.com>
2020-12-16 12:16:22 -0500

    added class

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FunWith.NET5
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public object MyProperty { get; set; }
+        public Student()
+        {
+        }
+    }
+}
