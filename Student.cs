b6140ed7165547e49f80ef69270cc3200ba5e5f5
elenavonder <evonder13@gmail.com>
2020-12-16 12:22:30 -0500

    added properties and method

--- Student.cs
+++ Student.cs
@@ -5,7 +5,18 @@ namespace FunWith.NET5
     {
 
         public int Id { get; set; }
-        public object MyProperty { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int SAT { get; set; }
+        public double GPA { get; set; }
+        public int? MajorId { get; set; }
+        public Major Major { get; set; }
+
+        public string Fullname()
+        {
+            var fullname = $"{this.Lastname}, {this.Firstname}";
+            return fullname;
+        }
         public Student()
         {
         }
